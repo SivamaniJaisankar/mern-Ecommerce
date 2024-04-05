2c635fcdef8de5dc438630a20cd22555499ddfaa
SivamaniJaisankar <sivamaniii1123@gmail.com>
2024-04-05 12:16:51 +0530

    register screen

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,7 @@ import { Provider } from "react-redux";
 import store from "./app/store.js";
 import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
--- client/src/screens/RegisterScreen.jsx
+++ client/src/screens/RegisterScreen.jsx
@@ -1,41 +1,55 @@
 import React, { useState } from "react";
-import { useLoginMutation } from "../app/slices/userApiSlice";
+import { useRegisterMutation } from "../app/slices/userApiSlice";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../app/slices/userSlice";
 import { Link, useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
 import { toast } from "react-toastify";
 
-export default function LoginScreen() {
+export default function RegisterScreen() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  const [login, { isLoading }] = useLoginMutation();
+  const [register, { isLoading }] = useRegisterMutation();
 
-  const handleLogin = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
-    try {
-      const res = await login({ email, password }).unwrap();
-      console.log(res);
-      dispatch(setCredentials({ ...res }));
-      navigate("/");
-      toast.success("Login Successful");
-    } catch (error) {
-      console.log(error);
-      toast.error(error?.data?.message || error?.message);
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        console.log(res);
+        dispatch(setCredentials({ ...res }));
+        navigate("/");
+        toast.success("Register Successful");
+      } catch (error) {
+        console.log(error);
+        toast.error(error?.data?.message || error?.message);
+      }
     }
   };
 
   return (
     <div className="bg-gray-100 shadow-lg shadow-gray-500 rounded-xl container mx-auto max-w-md mt-12 mb-4">
       <h2 className="py-3 mb-2 text-center text-xl text-gray-500 font-semibold uppercase">
-        Login
+        Register
       </h2>
-      <form className="flex flex-col mx-2" onSubmit={handleLogin}>
+      <form className="flex flex-col mx-2" onSubmit={handleRegister}>
         <input
           className="mt-2 mb-5 px-2 py-3 rounded-md shadow-xl bg-gray-100 outline-none hover:shadow-slate-400"
+          type="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          className="mt-2 mb-5 px-2 py-3 rounded-md shadow-xl bg-gray-100 outline-none hover:shadow-slate-400"
+          type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
@@ -47,25 +61,29 @@ export default function LoginScreen() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <h6 className="my-3 font-thin font-sans hover:cursor-pointer">
-          Forget Password ? <span className="text-blue-500">Click Here</span>
-        </h6>
+        <input
+          className="mt-2 mb-5 px-2 py-3 rounded-md shadow-xl bg-gray-100 outline-none hover:shadow-slate-400"
+          placeholder="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <div className="mb-1 py-1 flex justify-around max-w-sm">
           <button
             type="submit"
             className="px-6 py-1 outline-none border-none bg-red-500 text-white uppercase font-semibold rounded-sm hover:shadow-xl hover:shadow-red-300"
             disabled={isLoading}
           >
-            {isLoading ? "Loading..." : "Login"}
+            {isLoading ? "Loading..." : "Register"}
           </button>
           <button className="px-6 py-1 outline-none border-none bg-blue-400 text-white uppercase font-semibold rounded-sm hover:shadow-xl hover:shadow-blue-300">
-            Sign in With Google
+            Sign up With Google
           </button>
         </div>
         <h6 className="mt-1 mb-4 font-thin font-sans hover:cursor-pointer">
-          Don't have an account ?
-          <Link to="/register">
-            <span className="text-blue-500">Register Here</span>
+          Already have an account ?{" "}
+          <Link to="/login">
+            <span className="text-blue-500">Sign in</span>
           </Link>
         </h6>
         {isLoading && (
